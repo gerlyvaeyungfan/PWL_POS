20299f06780c7f108cabb907239192c622a18b9a
gerly <gherly.ylrehg@gmail.com>
2025-04-20 06:39:22 +0700

    Js3 - Praktikum5

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,45 @@
 <?php
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\SalesController;
+?>
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 // Halaman Home
-Route::get('/', function(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View {
+Route::get('/', function(){
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+
+
+
+
+
+
 
 
 // Halaman Home
-Route::get('/', [HomeController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index']);
 
 // Halaman Products dengan prefix category
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
+// Route::prefix('category')->group(function () {
+// Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+// Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+// Route::get('/home-care', [ProductController::class, 'homeCare']);
+// Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// });
 
 // Halaman User dengan parameter ID dan Name
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
 // Halaman Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
+// Route::get('/sales', [SalesController::class, 'index']);
