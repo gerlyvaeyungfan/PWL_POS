2a5f1718911921014e776206f44c65337a326eea
gerly <gherly.ylrehg@gmail.com>
2025-04-21 11:32:49 +0700

    Js4 - Praktikum2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,18 +4,6 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1><strong>Data User</strong></h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr>
-    </table>
-</body>
-</html>
-{{-- <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
@@ -29,6 +17,19 @@
             <td>{{ $data->username }}</td>
             <td>{{ $data ->nama }}</td>
             <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
+</html>
+
+{{-- <body>
+    <h1><strong>Data User</strong></h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
         </tr>
     </table>
 </body> --}}
