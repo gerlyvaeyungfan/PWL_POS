48956431ec951f1964c69d407ee9cece295ce758
gerly <gherly.ylrehg@gmail.com>
2025-04-22 06:57:23 +0700

    Js5 - Praktikum3

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 // use App\Http\Controllers\ProductController;
 // use App\Http\Controllers\UserController;
 // use App\Http\Controllers\SalesController;
+// use App\Http\Controllers\KategoriController;
 ?>
 
 <?php
@@ -10,11 +11,21 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 
 // Halaman Home
 Route::get('/',[WelcomeController::class,'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);         // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);     // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);  // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);        // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);      // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);    // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
